cbfea46e27fd5949626f5f38dc2e2e0843683c48
Gedeonobae <gideonobae@gmail.com>
2021-11-15 18:34:24 +0300

    prototypes of all my functions

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,7 +1,7 @@
+
 #ifndef LISTS_H
 #define LISTS_H
-
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -13,8 +13,8 @@
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+  int n;
+  struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
