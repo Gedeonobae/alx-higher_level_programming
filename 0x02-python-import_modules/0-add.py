02b728bc3ff59a7b250a8b93bc4275ff8e306834
Gedeonobae <gideonobae@gmail.com>
2021-11-18 21:04:15 +0300

    that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 0_add.py
+# Gedeon Obae Gekonge <gideonobae@gmail.com>
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b))):x
