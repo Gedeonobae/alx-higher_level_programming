e0f38279c5e640fea4a0fd95e571a2f2b692812a
Gedeonobae <gideonobae@gmail.com>
2021-11-21 15:21:03 +0300

    that adds 2 tuples.

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 7-add_tuple.py
+# Gedeon Obae Gekonge <gideonobae@gmail.com.com>
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    a = len(tuple_a)
+    b = len(tuple_b)
+
+    sums = ((tuple_a[0] if a > 0 else 0) + (tuple_b[0] if b > 0 else 0),
+            (tuple_a[1] if a > 1 else 0) + (tuple_b[1] if b > 1 else 0))
+
+    return sums
+
+# tuple_a += (0, 0)
+# tuple_b += (0, 0)
+# return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
