a8563447dcfd2f8291afcd1233429a740adcad1b
Gedeonobae <gideonobae@gmail.com>
2021-11-21 18:08:53 +0300

    a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,77 @@
+/*
+ * File: 13-is_palindrome.c
+ * Auth: Gedeon Obae Gekonge
+ */
+
 #include "lists.h"
 
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
 /**
- * is_palindrome - determine if singly linked list is palindrome
- * @head: pointer to head of singly linked list
- * Return: 0 if not, 1 if palindrome
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
-  listint_t *tmp = *head;
-  unsigned int size = 0, i = 0;
-  int data[10240];
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-  if (head == NULL) /* non-existing list is not */
-    return (0);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-  if (*head == NULL) /* empty list is palindrome */
-    return (1);
+	tmp = *head;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
+	}
 
-  while (tmp) /* find size of linked list */
-    {
-      tmp = tmp->next;
-      size += 1;
-    }
-  if (size == 1) /* single node list is palindrome */
-    return (1);
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
 
-  tmp = *head;
-  while (tmp) /* pull node data into array to compare */
-    {
-      data[i++] = tmp->n;
-      tmp = tmp->next;
-    }
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
 
-  for (i = 0; i <= (size/2); i++)
-    {
-      if (data[i] != data[size - i - 1])
-	return (0);
-    }
-  return (1);
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
+	return (1);
 }
