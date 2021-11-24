d1028b7f39ca2f76908e7749638d90644e2d9c99
Gedeonobae <gideonobae@gmail.com>
2021-11-24 23:23:26 +0300

    that returns the number of keys in a dictionary.

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+# Gedeon Obae Gekonge <gideonobae@gmail.com>
+
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
