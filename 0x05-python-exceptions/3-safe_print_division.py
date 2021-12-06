e5ced96c3b02f5dc717bc74e98033e9270575cfc
Gedeonobae <gideonobae@gmail.com>
2021-12-06 17:24:34 +0300

    that divides 2 integers and prints the result.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-# 2-safe_print_list_integers.py
+# 3-safe_print_division.py
 # Gedeon Obae Gekonge <gideonobae@gmail.com>
 
 
-def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x elements of a list that are integers.
-    Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
-    Returns:
-        The number of elements printed.
-    """
-    ret = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            ret += 1
-        except (ValueError, TypeError):
-            continue
-    print("")
-    return (ret)
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
