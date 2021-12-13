7ed05c4bfcef61a0a8a0f401ab34c94b7cf64dab
Gedeonobae <gideonobae@gmail.com>
2021-12-13 20:39:40 +0300

    that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Gedeon Obae Gekonge <gideonobae@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
