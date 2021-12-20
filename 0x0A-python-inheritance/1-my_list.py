c9adba321ad3fd1a19db020e314ff4cf57570958
Gedeonobae <gideonobae@gmail.com>
2021-12-21 02:19:29 +0300

    that inherits from list:

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 1-my_list.py
+# Gedeon Obae Gekonge <gideonobae@gmailcom>
+"""Defines an inherited list class MyList."""
+
+
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
+
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
