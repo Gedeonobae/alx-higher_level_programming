d8e131fac754c761df30d05259e47e5d7cbc68ea
Gedeonobae <gideonobae@gmail.com>
2021-12-21 21:38:28 +0300

    that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 11-square.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Gedeon Obae Gekonge <gideoobae@gmail.com>
 """Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
