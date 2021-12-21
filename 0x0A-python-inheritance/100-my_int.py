43273177ca52c9847fea5222b588550e3b25c69e
Gedeonobae <gideonobae@gmail.com>
2021-12-21 08:41:22 +0300

    that inherits from int:

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
-# 100-my_int.py
-# Amos Mwongela Gabriel <amosmwongelah@gmail.com> 
-"""Defines a class MyInt that inherits from int."""
+"""
+Module 100-my_int
+Contains class MyInt that inherits from int
+Sneaky --> has == and != operators inverted!
+"""
 
 
 class MyInt(int):
-    """Invert int operators == and !=."""
+    """
+    Methods:
+        __init__(self, num)
+        __eq__(self, other)
+        __ne__(self, other)
+    """
+    def __init__(self, num):
+        """initialize num"""
+        self.num = num
 
-    def __eq__(self, value):
-        """Override == opeartor with != behavior."""
-        return self.real != value
+    def __eq__(self, other):
+        """
+        Return:
+           True if both not equal
+        """
+        return self.num != other
 
-    def __ne__(self, value):
-        """Override != operator with == behavior."""
-        return self.real == value
+    def __ne__(self, other):
+        """
+        Return:
+           True if both equal
+        """
+        return self.num == other
