322fb666dfea2554995c54294971ad79eca10ba7
Gedeonobae <gideonobae@gmail.com>
2021-12-21 20:21:28 +0300

    that returns a list of lists of integers representing the Pascal’s triangle of n:

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module 14-pascal_triangle
+Contains function that returns int lists of pascal triangle of any given size
+"""
+
+
+def pascal_triangle(n):
+    """
+    Return:
+        empty list [] if n <= 0
+        if n is 7, we should expect:
+            [1]
+            [1, 1]
+            [1, 2, 1]
+            [1, 3, 3, 1]
+            [1, 4, 6, 4, 1]
+            [1, 5, 10, 10, 5, 1]
+            [1, 6, 15, 20, 15, 6, 1]
+    """
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+
+    triangle = [[1]]
+    for rows in range(n-1):
+        triangle.append([a+b for a, b
+                         in zip([0] + triangle[-1], triangle[-1] + [0])])
+    return triangle
