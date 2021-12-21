63d7a06d55c7077509b0c5229ee0af0b8c8ca754
Gedeonobae <gideonobae@gmail.com>
2021-12-21 21:24:31 +0300

    that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 4-inherits_from.py
-# Amos Mwongela Gabriel <amosmwongelah@gmail.com>
+# Gedeon Obae Gekonge <gideonobae@gmail.com>
 """Defines an inherited class-checking function."""
 
 
