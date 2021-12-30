0c4cbb4c7dcb807d576434dc8b83d3a8052f5c5f
Gedeonobae <gideonobae@gmail.com>
2021-12-31 01:40:46 +0300

    add test_square.py file

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # test_square.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Gedeon Obae Gekonge <gideonobae@gmail.com>
 """Defines unittests for models/square.py.
 
 Unittest classes:
