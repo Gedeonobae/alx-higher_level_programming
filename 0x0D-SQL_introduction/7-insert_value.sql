860d3575cd563e4a8f331cee7c1fc426398df6bf
Gedeonobae <gideonobae@gmail.com>
2022-02-02 20:40:51 +0300

    that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- insert a new row of data in this table in this database
+
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School')
