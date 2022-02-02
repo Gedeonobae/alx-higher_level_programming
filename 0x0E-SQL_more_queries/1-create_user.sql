913a64c76674999fdc58f5e4aa009e8c2e51d9d9
Gedeonobae <gideonobae@gmail.com>
2022-02-02 20:00:28 +0300

    that creates the MySQL server user user_0d_1.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,8 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+-- creates the MySQL server user user_0d_1
+-- should have all privileges
+-- password should be set to user_0d_1_pwd
+-- if user_0d_1 already exists, script should not fail
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
