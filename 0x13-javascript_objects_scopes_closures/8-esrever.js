4161eceb5406e7a15a484259debe52fbf66c589c
Gedeonobae <gideonobae@gmail.com>
2022-03-02 21:19:50 +0300

    that returns the reversed version of a list:

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-// JS Script
 exports.esrever = function (list) {
-  let res = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    res.push(list[i]);
-  }
-  return res;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
