c16b14e8721920c3c2e6132ac36fe9a239f70990
Gedeonobae <gideonobae@gmail.com>
2022-03-07 04:58:24 +0300

    that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    script that lists all states with a name starting
+    with N (upper N) from the database hbtn_0e_0_usa
+"""
+from sys import argv
+import MySQLdb
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    cur = conn.cursor()
+    try:
+        stmt = """
+        SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC
+        """
+        cur.execute(stmt)
+        rtn = cur.fetchall()
+    except MySQLdb.Error:
+        try:
+            rtn = ("MySQLdb Error")
+        except IndexError:
+            rtn = ("MySQLdb Error - IndexError")
+    for i in rtn:
+        print(i)
+    cur.close()
+    conn.close()
