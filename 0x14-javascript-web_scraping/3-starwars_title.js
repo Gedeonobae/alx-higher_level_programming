63cf8e34171efdf8ee35cda9a5abca84abcb7b38
Gedeonobae <gideonobae@gmail.com>
2022-04-13 02:39:57 +0300

    script that prints the title of a Star Wars movie where the episode number matches a given integer.

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+
+#!/usr/bin/node
+const request = require('request');
+let url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
